f3c5c2ca717c9127b3f4c88636c7c6a41e17734d
razboy <mishaneiman@gmail.com>
2025-01-05 21:21:48 -0500

    Trying my hand at making a midi synth

---
+++ examples/Zynth/build.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+const arbor = @import("arbor");
+
+pub fn build(b: *std.Build) !void {
+    try arbor.addPlugin(b, .{
+        .description = .{
+            .name = "Synth",
+            .id = "com.BugsAplenty.Synth",
+            .company = "BugsAplenty",
+            .version = "0.1.0",
+            .copyright = "MIT",
+            .url = "",
+            .manual = "",
+            .contact = "",
+            .description = "A simple synthesizer",
+        },
+        .features = arbor.features.STEREO | arbor.features.SYNTH |
+            arbor.features.GUI,
+        .root_source_file = "plugin.zig",
+        .target = b.standardTargetOptions(.{}),
+        .optimize = b.standardOptimizeOption(.{}),
+    });
+}
